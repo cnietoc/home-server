537e1131ce300ded4a2f039f95f738308191c32d
Carlos Nieto <carlos.nietodelacuesta@telefonica.com>
2025-11-27 13:19:03 +0100

    YQ fix

--- lib/yq.sh
+++ lib/yq.sh
@@ -5,17 +5,48 @@
     exit 1
 }
 
-# === Funciones Wrapper para Snap ===
+# === Detección de instalación ===
+# Detectar si yq está instalado via snap
+yq::is_snap() {
+    command -v yq >/dev/null 2>&1 || return 1
+    local yq_path
+    yq_path=$(command -v yq)
+    [[ "$yq_path" =~ /snap/ ]]
+}
+
+# Ejecutar yq (usando Docker si está en snap)
+yq::exec() {
+    if yq::is_snap; then
+        # Ejecutar con Docker para evitar restricciones de snap
+        docker run --rm -i -v "${PWD}":/workdir mikefarah/yq "$@"
+    else
+        # Ejecutar yq nativo
+        yq "$@"
+    fi
+}
+
+# === Funciones Wrapper ===
 # Cuando yq se instala con Snap, tiene restricciones de acceso a archivos
-# Workaround: usar stdin en lugar de pasar el archivo como argumento
+# Workaround: usar Docker o stdin según la instalación
 
 # Leer de un archivo YAML (compatible con Snap)
 yq::read() {
     local query="$1"
     local file="$2"
 
-    # Usar stdin para evitar restricciones de Snap
-    yq eval "$query" < "$file"
+    if yq::is_snap; then
+        # Usar Docker con volumen montado
+        local abs_file
+        abs_file=$(realpath "$file")
+        local dir
+        dir=$(dirname "$abs_file")
+        local filename
+        filename=$(basename "$abs_file")
+        docker run --rm -i -v "$dir":/workdir mikefarah/yq eval "$query" "/workdir/$filename"
+    else
+        # Usar stdin para evitar problemas de permisos
+        yq eval "$query" < "$file"
+    fi
 }
 
 # Escribir a un archivo YAML in-place (compatible con Snap)
@@ -23,9 +54,21 @@ yq::write() {
     local query="$1"
     local file="$2"
 
-    # Usar stdin/stdout para evitar restricciones de Snap
-    local temp_file="${file}.tmp"
-    yq eval "$query" < "$file" > "$temp_file" && mv "$temp_file" "$file"
+    if yq::is_snap; then
+        # Usar Docker con volumen montado
+        local abs_file
+        abs_file=$(realpath "$file")
+        local dir
+        dir=$(dirname "$abs_file")
+        local filename
+        filename=$(basename "$abs_file")
+        local temp_file="${file}.tmp"
+        docker run --rm -i -v "$dir":/workdir mikefarah/yq eval "$query" "/workdir/$filename" > "$temp_file" && mv "$temp_file" "$file"
+    else
+        # Usar stdin/stdout para evitar problemas de permisos
+        local temp_file="${file}.tmp"
+        yq eval "$query" < "$file" > "$temp_file" && mv "$temp_file" "$file"
+    fi
 }
 
 # Alias para compatibilidad con código existente
